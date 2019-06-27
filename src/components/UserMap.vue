bf6dbb8068c757483ae5d4f51ab1e8de1a1122a0
Soon Won Choi <wony6731@naver.com>
2019-06-28 00:31:15 +0900

    마우스 버튼 효과 추가

--- src/components/UserMap.vue
+++ src/components/UserMap.vue
@@ -82,7 +82,7 @@
         name: "UserMap",
         components: {Hamburger},
         created(){
-          console.log(this.$store.state.user_id);
+          console.log("http.get method");
           this.$http.get(`http://ec2-18-191-120-181.us-east-2.compute.amazonaws.com:8080/api/solvedProblems/list/${this.$store.state.user_id}`)
           .then(res => {
             console.log(res);
